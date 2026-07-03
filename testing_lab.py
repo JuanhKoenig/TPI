7a1dc36e3d4c4cfbad09da23f5b9258bdcccff44
Koenig Juan <koenigjuan@gmail.com>
2026-07-03 17:15:51 -0300

    commit que no sirve

--- testing_lab.py
+++ testing_lab.py
@@ -1,26 +1,7 @@
-lista = [
-    {"nombre": "Lala", "edad": 30},
-    {"nombre": "Juan", "edad": 28},
-    {"nombre": "Pedro", "edad": 29},
-    {"nombre": "Sofía", "edad": 22},
-    {"nombre": "Lolo", "edad": 31},
-    {"nombre": "Ana", "edad": 25},
-    {"nombre": "Carlos", "edad": 35},
-    {"nombre": "María", "edad": 27}
-]
+continentes = ["America", "Africa", "Oceania", "Europa", "Asia", "Antartida"]
 
+for i in range(len(continentes)):
+    index = i + 1
+    print(f"{index}) {continentes[i]}")
 
-testing = lista[-1]["edad"]
-menor = lista[0]
-for item in range(1, len(lista)):
-
-    anterior = lista[item - 1]
-    anterior_edad = anterior["edad"]
-    
-    actual = lista[item]
-    actual_edad = actual["edad"]
-
-    if actual["edad"] < menor["edad"]:
-        menor = actual
-    
-print(f"el menor es {menor["nombre"]} con {menor["edad"]} años")
+seleccion = int(input(">"))
